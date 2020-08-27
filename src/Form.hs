6b837ffe2bdef81fd56c120d0bd2a7b007d7cd5f
Lyndon Maydwell <lyndon@sordina.net>
2020-08-27 22:57:07 +1000

    allow moving items

--- src/Form.hs
+++ src/Form.hs
@@ -6,23 +6,19 @@
 
 module Form where
 
+import           Control.Arrow            (left)
+import           Lens.Micro               (Lens', lens, (&), (.~))
+import           System.Exit              (ExitCode (..))
+import           Data.List                (intercalate, nub)
+import           Data.List.Split
+import           Data.Maybe
+import           Lens.Micro.TH
+import           Control.Monad.IO.Class
+
 import qualified Control.Concurrent.Async as Async
-import qualified Data.Map                 as M
 import qualified Data.Text                as T
 import qualified System.Environment       as Env
 import qualified System.Process           as P
-
-import           Control.Arrow            (left)
-import           Control.Monad.IO.Class
-import           Lens.Micro               (Lens', lens, (&), (.~))
-import           Lens.Micro.TH
-import           System.Exit              (ExitCode (..))
-
-import qualified Graphics.Vty             as V
-
-import           Data.List                (intercalate)
-import           Data.List.Split
-import           Data.Maybe
 
 import Brick       ((<+>), (<=>))
 import Brick.Forms ((@@=))
@@ -33,16 +29,17 @@ import qualified Brick.Widgets.Edit    as Brick
 import qualified Brick.BChan           as Brick
 import qualified Brick.Focus           as Brick
 import qualified Brick.Forms           as Brick
+import qualified Graphics.Vty          as V
 
 -- Types and Lenses
 
 data Name
-  = Segment String
+  = Segment Int
   | Command
   | OutPort
   deriving (Eq, Ord, Show)
 
-type PathSegments = M.Map String Bool
+type PathSegments = [(String, Bool)]
 
 data Hide a = Hide { unHide :: a }
 
@@ -63,8 +60,7 @@ data AppState = AppState
 makeLenses ''AppState
 
 seg :: String -> Lens' PathSegments Bool
-seg k = lens (fromMaybe False . M.lookup k) (\m b -> M.insert k b m)
-
+seg k = lens (fromMaybe False . lookup k) (\m b -> map (\(x,y) -> if x == k then (x,b) else (x,y)) m)
 
 -- Forms
 
@@ -73,10 +69,10 @@ mkForm state =
     let label s w = Brick.padBottom (Brick.Pad 1) $ (Brick.vLimit 1 $ Brick.hLimit 15 $ Brick.str s <+> Brick.fill ' ') <+> w
     in flip Brick.newForm state $
       [ label "Command" @@= Brick.editTextField command Command (Just 1)
-      ] ++ map (makeSegInput) (M.toList $ _segments state)
+      ] ++ zipWith makeSegInput [0..] (_segments state)
 
   where
-  makeSegInput (k, _v) = Brick.checkboxField (segments . seg k) (Segment k) (T.pack k)
+  makeSegInput i (k, _v) = Brick.checkboxField (segments . seg k) (Segment i) (T.pack k)
 
 style :: Brick.AttrMap
 style = Brick.attrMap V.defAttr
@@ -86,19 +82,18 @@ style = Brick.attrMap V.defAttr
   , (Brick.focusedFormInputAttr, V.black `Brick.on` V.yellow)
   ]
 
-help :: String
-help = "(Keys: Up/Down/Left/Right/C-j/C-k)"
-
 draw :: Brick.Form AppState e Name -> [Brick.Widget Name]
 draw f = [ Brick.padTop (Brick.Pad 2) (Brick.hCenter form) <=> Brick.hCenter outW]
   where
+    helpC  = " Path Explorer ~ (Keys: Enter/Space/Tab/C-n/C-p/C-u/C-d) "
+    helpO  = " ~ (Keys: Up/Down/Left/Right/C-j/C-k/C-l) "
     out "" = ""
     out o  = "\n\nSTDOUT:\n\n" <> o
     err "" = ""
     err e  = "\n\nSTDERR:\n\n" <> e
-    form   = Brick.borderWithLabel (Brick.str " Path Explorer ~ (Keys: Enter/Space/Tab/C-n/C-p) ")
+    form   = Brick.borderWithLabel (Brick.str helpC)
            $ Brick.hLimit 88 $ Brick.renderForm f
-    outW   = Brick.padTop (Brick.Pad 1) $ Brick.borderWithLabel (Brick.str (" " <> fst body <> " ~ " <> help <> " "))
+    outW   = Brick.padTop (Brick.Pad 1) $ Brick.borderWithLabel (Brick.str (" " <> fst body <> helpO))
            $ Brick.vLimit 44 $ Brick.hLimit 88 (Brick.viewport OutPort Brick.Vertical (Brick.str (snd body)))
     body   = case _output (Brick.formState f) of
                Right (ExitSuccess, o, _) -> ("Success", o)
@@ -119,6 +114,21 @@ setFF s s' =
     Nothing -> s'
     Just f  -> Brick.setFormFocus f s'
 
+reorder :: Int -> Brick.Form AppState e Name -> Brick.EventM n (Brick.Next (Brick.Form AppState e Name))
+reorder d s = case Brick.focusGetCurrent $ Brick.formFocus s of
+  Just (Segment n) ->
+    let a = reorderL n d o
+    in s & Brick.formState & segments .~ a & mkForm & Brick.setFormFocus (Segment (n+d)) & Brick.continue
+  _ -> Brick.continue s
+  where
+    o = s & Brick.formState & _segments
+
+reorderL :: Int -> Int -> [a] -> [a]
+reorderL n d o =
+  case splitAt n o of
+    (x,y:z) -> let (x',z') = splitAt (n+d) (x++z) in x' ++ [y] ++ z'
+    _       -> o
+
 eventHandler :: Bus -> Brick.Form AppState e Name -> Brick.BrickEvent Name e -> Brick.EventM Name (Brick.Next (Brick.Form AppState e Name))
 eventHandler _ s (Brick.VtyEvent (V.EvResize {}))    = Brick.continue s
 eventHandler _ s (Brick.VtyEvent (V.EvKey V.KEsc _)) = Brick.halt s
@@ -137,18 +147,25 @@ eventHandler chan s e = do
 
   case e of
     Brick.VtyEvent (V.EvKey (V.KChar 'q') _) | cf /= Just Command -> Brick.halt s
-    Brick.VtyEvent (V.EvKey (V.KChar 'c') [V.MCtrl])              -> Brick.halt s
-    Brick.VtyEvent (V.EvKey (V.KChar 'n') [V.MCtrl])              -> Brick.continue $ Brick.setFormFocus nf s'
-    Brick.VtyEvent (V.EvKey (V.KChar 'p') [V.MCtrl])              -> Brick.continue $ Brick.setFormFocus pf s'
-    Brick.VtyEvent (V.EvKey (V.KChar 'k') [V.MCtrl])              -> Brick.vScrollBy sp (-1)  >> Brick.continue s'
-    Brick.VtyEvent (V.EvKey (V.KChar 'j') [V.MCtrl])              -> Brick.vScrollBy sp 1     >> Brick.continue s'
-    Brick.VtyEvent (V.EvKey (V.KChar 'l') [V.MCtrl])              -> Brick.vScrollBy sp 1     >> Brick.continue s'
-    Brick.VtyEvent (V.EvKey V.KUp _)                              -> Brick.vScrollBy sp (-1)  >> Brick.continue s'
-    Brick.VtyEvent (V.EvKey V.KDown _)                            -> Brick.vScrollBy sp 1     >> Brick.continue s'
-    Brick.VtyEvent (V.EvKey V.KLeft _)                            -> Brick.vScrollBy sp (-10) >> Brick.continue s'
-    Brick.VtyEvent (V.EvKey V.KRight _)                           -> Brick.vScrollBy sp 10    >> Brick.continue s'
-    Brick.VtyEvent (V.EvKey V.KEnter _)                           -> Brick.setFormFocus Command s' & Brick.continue
-    _ | psegs == segs s' && c == c'                               -> Brick.continue s'
+
+    Brick.VtyEvent (V.EvKey (V.KChar 'c') [V.MCtrl]) -> Brick.halt s
+    Brick.VtyEvent (V.EvKey (V.KChar 'n') [V.MCtrl]) -> Brick.continue $ Brick.setFormFocus nf s'
+    Brick.VtyEvent (V.EvKey (V.KChar 'p') [V.MCtrl]) -> Brick.continue $ Brick.setFormFocus pf s'
+    Brick.VtyEvent (V.EvKey (V.KChar 'k') [V.MCtrl]) -> Brick.vScrollBy sp (-1) >> Brick.continue s'
+    Brick.VtyEvent (V.EvKey (V.KChar 'j') [V.MCtrl]) -> Brick.vScrollBy sp 1    >> Brick.continue s'
+    Brick.VtyEvent (V.EvKey (V.KChar 'l') [V.MCtrl]) -> Brick.vScrollBy sp 1    >> Brick.continue s'
+
+    Brick.VtyEvent (V.EvKey (V.KChar 'u') [V.MCtrl]) -> reorder (-1) s'
+    Brick.VtyEvent (V.EvKey (V.KChar 'd') [V.MCtrl]) -> reorder (1)  s'
+
+    Brick.VtyEvent (V.EvKey V.KUp _)    -> Brick.vScrollBy sp (-1)  >> Brick.continue s'
+    Brick.VtyEvent (V.EvKey V.KDown _)  -> Brick.vScrollBy sp 1     >> Brick.continue s'
+    Brick.VtyEvent (V.EvKey V.KLeft _)  -> Brick.vScrollBy sp (-10) >> Brick.continue s'
+    Brick.VtyEvent (V.EvKey V.KRight _) -> Brick.vScrollBy sp 10    >> Brick.continue s'
+    Brick.VtyEvent (V.EvKey V.KEnter _) -> Brick.setFormFocus Command s' & Brick.continue
+
+    _ | psegs == segs s' && c == c' -> Brick.continue s'
+
     _ -> do
       liftIO $ Async.cancel $ unHide $ _action $ Brick.formState $ s'
       a <- getOut chan c' (getPath s')
@@ -189,25 +206,16 @@ mkPath :: [String] -> String
 mkPath  = intercalate ":"
 
 getPath :: Brick.Form AppState e n -> [String]
-getPath = map fst . filter snd . M.toList . _segments . Brick.formState
+getPath = map fst . filter snd . _segments . Brick.formState
 
 initialCommand :: String
 initialCommand = "ls"
 
 main :: IO ()
 main = do
-    path <- splitOn ":" <$> Env.getEnv "PATH"
+    path <- nub . splitOn ":" <$> Env.getEnv "PATH" -- Remove duplicates
     chan <- Brick.newBChan 1 -- Event prompting channel
     ls   <- getOut chan initialCommand path
-
-    -- Wake up the eventloop to check for async actions periodically
-    -- Not required now that `getOut` signals its status
-    {-
-    _ <- Async.async do
-          forever do
-            Conc.threadDelay 100000
-            BChan.writeBChanNonBlocking chan ()
-            -}
 
     let
       buildVty = do
@@ -218,7 +226,7 @@ main = do
       state =
           AppState
             { _command  = T.pack initialCommand
-            , _segments = M.fromList $ zip path (repeat True)
+            , _segments = zip path (repeat True)
             , _action   = ls
             , _output   = Left "Nothing Yet!"
             }
